424763c91eb24f4bc86889e908474b1779c19256
codingwith3dv <deveshvdddas@gmail.com>
2021-10-04 11:54:20 +0530

    :poop: Element not rendering, issue needs to be fixed

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,18 @@
-import {
+import { sculpterDefine, SculpterElement } from './sculpter-element/sculpter-define';
+import { Tokenizer } from './sculpter-parser/tokenizer';
+
+export {
   SculpterElement,
-} from './sculpter-element/sculpter-element';
-
-import {
   sculpterDefine
-} from './sculpter-element/sculpter-define';
+};
 
-@sculpterDefine('a-component')
+@sculpterDefine({
+  name: 'b-component',
+  templateUrl: '<p>Hello World</p>',
+  stylesUrl: ''
+})
 class A extends SculpterElement {
   constructor() {
     super();
-    const shadow = this.attachShadow({mode: 'open'});
-    
-    const content = document.createElement('p');
-    content.innerHTML = 'Hello futher mucker';
-    shadow.appendChild(content);
   }
 }
